16f8b72055f092adf17f84769b659ac6c0aba244
David Tembo <dtembo965@gmail.com>
2020-02-06 17:14:11 -1000

    Updates

---
+++ precache-manifest.bdce5b24be75122c68bcc94884b995c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea69f9629e924beaa708004fe96a19e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "daff0b4f12944286ae5c",
+    "url": "/monsters-rolodex/static/css/main.6f21d8be.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daff0b4f12944286ae5c",
+    "url": "/monsters-rolodex/static/js/main.e2db4f1c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
